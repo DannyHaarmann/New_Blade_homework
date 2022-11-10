647ef9d332f93a6c13e90485c736bcdcbfa234c7
DannyHaarmann <94137078+DannyHaarmann@users.noreply.github.com>
2022-11-10 17:25:48 -0600

    singly linked list

---
+++ HomeWork_blade/src/DataLinkedList.java
@@ -0,0 +1,17 @@
+
+public class DataLinkedList 
+{
+	public static class Node {
+	    int data; 
+	    Node nextNode;
+ 
+	    public Node(int data) {
+	        this.data = data;
+	        this.nextNode = null;       
+	    }
+ 
+	    public int getData() {
+	        return this.data;
+	    }
+	} // Node class
+}
