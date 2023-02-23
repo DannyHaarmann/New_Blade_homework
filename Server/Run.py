5791645b4704e796be5b0877af2c42f4fc7c5e89
DannyHaarmann <94137078+DannyHaarmann@users.noreply.github.com>
2023-02-22 22:24:41 -0600

    next push with more info

--- Server/Run.py
+++ Server/Run.py
@@ -1,16 +1,80 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
+Server = ['']
+
 class helloHandler(BaseHTTPRequestHandler):
 	def do_GET(self):
-		self.send_response(200)
-		self.send_header('content-type' , 'text/html')
-		self.end_headers()
-		self.wfile.write('Hello' .encode()) 
+		if self.path.endswith('/Server'):
+			self.send_response(200)
+			self.send_header('content-type' , 'text/html')
+			self.end_headers()
+		
+
+			output = ''
+			output += '<html><body>'
+		
+			output += '<h1>Hello welcome to the most advanced web page</h1>'
+			output += '<h3><a href="/Server/Signup"> Sign Up</a></h3>'
+			output += '<h3><a href="/Server/Login"> Log In</a></h3>'
+			for do in Server:
+					output += do
+					output += '</br>'
+			output += '</body></html>'
+			self.wfile.write(output.encode())
 
 
+		if self.path.endswith('/Login'):
+			self.send_response(200)
+			self.send_header('content-type' , 'text/html')
+			self.end_headers()
+			
+			output = ''
+			output += '<html><body>'
+			output += '<h1> Login </h1>'
+
+			output += '<form method="POST" enctype="multipart/form-data" action="/Server/Login">'
+			output += '<input name="task" type="text" placeholder="Email">'
+			output += '</form>'
+
+			output += '<form method="POST" enctype="multipart/form-data" action="/Server/Login">'
+			output += '<input name="task" type="text" placeholder="Password">'
+			output += '<input type="submit" value="submit">'
+			output += '</form>'
+
+			output += '</body></html>'
+
+			self.wfile.write(output.encode())
+
+		if self.path.endswith('/Signup'):
+			self.send_response(200)
+			self.send_header('content-type' , 'text/html')
+			self.end_headers()
+
+			output = ''
+			output += '<html><body>'
+			output += '<h1> Sign up Here </h1>'
+			
+			output += '<form method="POST" enctype="multipart/form-data" action="/Server/Signup">'
+			output += '<input name="task" type="text" placeholder="First name">'
+			output += '</form>'
+
+			output += '<form method="POST" enctype="multipart/form-data" action="/Server/Signup">'
+			output += '<input name="task" type="text" placeholder="last name">'
+			output += '</form>'
 
 
+			output += '<form method="POST" enctype="multipart/form-data" action="/Server/Signup">'
+			output += '<input name="task" type="text" placeholder="Email">'
+			output += '</form>'
+				
+			output += '<form method="POST" enctype="multipart/form-data" action="/Server/Signup">'
+			output += '<input name="task" type="text" placeholder="Password">'
+			output += '<input type="submit" value="submit">'
+			output += '</form>'
 
+			output += '</body></html>'
+
+			self.wfile.write(output.encode())
 
 def main():
 		PORT = 8000
@@ -20,4 +84,3 @@ def main():
 
 if __name__ == '__main__':
 	main()
-
